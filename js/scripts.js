8d440b40372a6facc5642ec740603e596358d80e
Dan Danilyuk and Nate McGregor <dan@>
2017-08-22 15:40:48 -0700

    add withdrawl/deposit logic and initialize variables

--- js/scripts.js
+++ js/scripts.js
@@ -10,17 +10,28 @@ bankAccount.prototype.withDep = function (add,sub) {
 };
 
 bankAccount.prototype.viewBal = function () {
-  return "$" + this.balance;
+  return this.balance;
 };
 
 //Front End
 $(document).ready(function() {
   var newAccount;
+  var deposit = parseInt($("#deposit").val("0"));
+  var withdrawl = parseInt($("#withdrawl").val("0"));
+//Input Form
   $("form#register-form").submit(function(event) {
     event.preventDefault();
     var name = $("#name").val();
     var initial = parseInt($("#initial").val());
     newAccount = new bankAccount(name,initial);
+    $("#output").text(newAccount.viewBal());
+  });
+//Bank Balance Form
+  $("form#deposit-form").submit(function(event) {
+    event.preventDefault();
+    deposit = parseInt($("#deposit").val());
+    withdrawl = parseInt($("#withdrawl").val());
+    newAccount.withDep(deposit,withdrawl);
     $("#output").text(newAccount.viewBal());
   });
 });
